5beb387d77a5bce4f9f08a8ae4a48c91d495e5b6
unknown <ghostlordwalker@gmail.com>
2022-07-17 10:58:42 +0100

    lets try and finsh up and post as working

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, avoid_print
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,100 +15,118 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var _taskController;
+
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _taskController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
+
+  @override
+  build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.indigo,
         title: Text(
           "Task Manager",
           style: GoogleFonts.montserrat(),
         ),
       ),
       body: Center(
-        child: Text("No task Added yet just for git", style: GoogleFonts.montserrat()),
+        child:
+            Text("Nothing to do for now :)", style: GoogleFonts.montserrat()),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) => Container(
-                    padding: EdgeInsets.all(20),
-                    height: 250,
-                    color: Colors.blueGrey,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Add Task",
-                                style: GoogleFonts.montserrat(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                )),
-                            GestureDetector(
-                                onTap: () => Navigator.of(context).pop(),
-                                child: Icon(Icons.close))
-                          ],
-                        ),
-                        Divider(thickness: 1),
-                        SizedBox(height: 20),
-                        TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5),
-                                  borderSide: BorderSide(color: Colors.blue)),
-                              fillColor: Colors.white,
-                              filled: true,
-                              hintText: "Enter Task",
-                              hintStyle: GoogleFonts.montserrat()),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          width: MediaQuery.of(context).size.width,
-                          child: Row(
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) => Container(
+                      padding: EdgeInsets.all(20),
+                      height: 250,
+                      color: Colors.indigo,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Container(
-                                width:
-                                    MediaQuery.of(context).size.width / 2 - 30,
-                                child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                        primary: Colors.white,
-                                        onPrimary: Colors.black),
-                                    onPressed: (() =>
-                                        print("reset button pressed")),
-                                    child: Text(
-                                      "Reset",
-                                      style: GoogleFonts.montserrat(),
-                                    )),
-                              ),
-                              Container(
-                                width:
-                                    MediaQuery.of(context).size.width / 2 - 30,
-                                child: ElevatedButton(
-                                    onPressed: (() =>
-                                        print("Add button pressed")),
-                                    child: Text(
-                                      "Add",
-                                      style: GoogleFonts.montserrat(),
-                                    )),
-                              )
+                              Text("Add Task",
+                                  style: GoogleFonts.montserrat(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                  )),
+                              GestureDetector(
+                                  onTap: () => Navigator.of(context).pop(),
+                                  child: Icon(Icons.close))
                             ],
                           ),
-                        )
-                      ],
-                    ),
-                  ));
-          print("modal pressed");
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.black,
-        ),
-        backgroundColor: Colors.purpleAccent,
-      ),
+                          Divider(thickness: 1),
+                          SizedBox(height: 20),
+                          TextField(
+                            controller: _taskController,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(color: Colors.blue)),
+                                fillColor: Colors.white,
+                                filled: true,
+                                hintText: "Enter Task",
+                                hintStyle: GoogleFonts.montserrat()),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            width: MediaQuery.of(context).size.width,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: MediaQuery.of(context).size.width / 2 -
+                                      30,
+                                  child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          primary: Colors.white,
+                                          onPrimary: Colors.black),
+                                      onPressed: (() =>
+                                          _taskController.text = ""),
+                                      child: Text(
+                                        "Reset",
+                                        style: GoogleFonts.montserrat(),
+                                      )),
+                                ),
+                                Container(
+                                  width: MediaQuery.of(context).size.width / 2 -
+                                      30,
+                                  child: ElevatedButton(
+                                      onPressed: (() =>
+                                          print("Add button pressed")),
+                                      child: Text(
+                                        "Add",
+                                        style: GoogleFonts.montserrat(),
+                                      )),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ));
+            print("modal pressed");
+          },
+          child: Icon(
+            Icons.add,
+            color: Colors.amber,
+            size: 35,
+          ),
+          backgroundColor: Colors.indigo),
     );
   }
 }
